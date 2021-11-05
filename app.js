b820f8fb509691dfb43bb5d20c51a308a12daf26
민경록 <67280967+kyr-min@users.noreply.github.com>
2021-11-05 10:02:39 +0900

    Update app.js

--- app.js
+++ app.js
@@ -9,5 +9,15 @@ socket.on('message', text => {
 document.querySelector('button').onclick = () => {
     const name = document.getElementById('name').value;
     const text = document.getElementById('message').value;
+    document.getElementById('message').value ="";
     socket.emit('message', name, text);
+}
+
+function enterkey() {
+    if (window.event.keyCode == 13) {
+        const name = document.getElementById('name').value;
+        const text = document.getElementById('message').value;
+        document.getElementById('message').value ="";
+        socket.emit('message', name, text);
+    }
 }
